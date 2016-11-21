4bbff2e1fd07fc0fc7fea9a8b77eec75bcce81dc
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2016-11-21 12:09:39 -0500

    Finding length of longest common subsequence

--- lcs.py
+++ lcs.py
@@ -3,3 +3,23 @@
 	Also, print the size of the longest common subsequence.
 '''
 
+A = 'bacdca'
+B = 'adbcda'
+
+n = len(A)
+m = len(B)
+
+opt = [[0 for i in range(0,n+2)] for j in range(0,m+2)]
+par = [[0 for i in range(0,n+2)] for j in range(0,m+2)]
+
+len(opt)
+for i in range(1,n+1):
+	for j in range(1,m+1):
+		if A[i-1] == B[j-1]:
+			opt[i][j] = opt[i-1][j-1] + 1
+		elif opt[i][j-1] >= opt[i-1][j]:
+			opt[i][j] = opt[i][j-1]
+		else:
+			opt[i][j] = opt[i-1][j]
+
+print opt[n][m]
