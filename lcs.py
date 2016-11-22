d018d0e2d642c4b910b4563e85e71cde7aab7493
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2016-11-22 10:37:50 -0500

    Organized code and comments added

--- lcs.py
+++ lcs.py
@@ -7,47 +7,61 @@
 		- 0 for diagnal
 '''
 
+def calculate_LCS(A,B):
+	n = len(A)
+	m = len(B)
+
+	#Opt array to store the optimal solution value till ith and jth position for 2 strings
+	opt = [[0 for i in range(0,m+1)] for j in range(0,n+1)]
+	#Pi array to store the direction when calculating the actual sequence
+	pi = [[0 for i in range(0,m+1)] for j in range(0,n+1)]
+
+	#Algorithm to calculate the length of the longest common subsequence
+	for i in range(1,n+1):
+		for j in range(1,m+1):
+			if A[i-1] == B[j-1]:
+				opt[i][j] = opt[i-1][j-1] + 1
+				pi[i][j] = 0
+			elif opt[i][j-1] >= opt[i-1][j]:
+				opt[i][j] = opt[i][j-1]
+				pi[i][j] = 1
+			else:
+				opt[i][j] = opt[i-1][j]
+				pi[i][j] = 2
+	#Length of the longest common subsequence is saved at opt[n][m]
+
+	#Algorithm to calculate the longest common subsequence using the Pi array
+	i = n
+	j = m
+	S = ''
+
+	while i>0 and j>0:
+		if pi[i][j] == 0:
+			S = A[i-1] + S
+			i-=1
+			j-=1
+		elif pi[i][j] == 2:
+			i-=1
+		else:
+			j-=1
+	return str(opt[n][m]),S
+
+#Path of Input and Output Files
 input_file = "input.txt"
 output_file = "output.txt"
+
+#Reading data from the input file
 file_in = open(input_file,"r")
 lines = file_in.readlines()
 file_in.close()
-file_out = open(output_file, "w")
-
 A = lines[0].strip()
 B = lines[1].strip()
 
-n = len(A)
-m = len(B)
+#Output File to write the data to
+file_out = open(output_file, "w")
 
-opt = [[0 for i in range(0,m+1)] for j in range(0,n+1)]
-par = [[0 for i in range(0,m+1)] for j in range(0,n+1)]
-
-for i in range(1,n+1):
-	for j in range(1,m+1):
-		if A[i-1] == B[j-1]:
-			opt[i][j] = opt[i-1][j-1] + 1
-			par[i][j] = 0
-		elif opt[i][j-1] >= opt[i-1][j]:
-			opt[i][j] = opt[i][j-1]
-			par[i][j] = 1
-		else:
-			opt[i][j] = opt[i-1][j]
-			par[i][j] = 2
-
-file_out.write(str(opt[n][m]))
-
-i = n
-j = m
-S = ''
-
-while i>0 and j>0:
-	if par[i][j] == 0:
-		S = A[i-1] + S
-		i-=1
-		j-=1
-	elif par[i][j] == 2:
-		i-=1
-	else:
-		j-=1
-file_out.write("\n" + S)
+#Calculating the LCS and it's length and writing to the output file
+length, sequence = calculate_LCS(A,B)
+file_out.write(length)
+file_out.write("\n" + sequence)
+file_out.close()
